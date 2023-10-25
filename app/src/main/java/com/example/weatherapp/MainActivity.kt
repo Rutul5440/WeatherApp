50c72e91bb7b9eba7f159c38089e0b7a29fb333d
rutul <rutulsuthar2018@gmail.com>
2023-10-26 00:09:09 +0530

    updated

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.weatherapp
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 // 548cd9aee7828f5f4642b9c341716422
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchWeatherData()
+    }
+
+    private fun fetchWeatherData() {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+        val response = retrofit.getWeatherData(city = "ahmedabad", "548cd9aee7828f5f4642b9c341716422", units = "metric")
+        response.enqueue(object : Callback<WeatherApp>{
+            override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null){
+                    val temperature = responseBody.main.temp.toString()
+                    Log.d( "TAG", "onResponse: $temperature")
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
+
+            }
+
+        })
     }
 }
