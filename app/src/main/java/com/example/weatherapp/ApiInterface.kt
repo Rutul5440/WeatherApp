52ff7eb5d55fe28317ee1bde8312d9929480f3a5
rutul <rutulsuthar2018@gmail.com>
2023-10-25 23:27:08 +0530

    added weather api and changes in gradle file

---
+++ app/src/main/java/com/example/weatherapp/ApiInterface.kt
@@ -0,0 +1,4 @@
+package com.example.weatherapp
+
+interface ApiInterface {
+}
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// 548cd9aee7828f5f4642b9c341716422
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
