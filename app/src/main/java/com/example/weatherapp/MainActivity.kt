7309087751c9699b3ed90359d6c386ce6d989e06
rutul <rutulsuthar2018@gmail.com>
2023-10-28 23:00:35 +0530

    set all Ids

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.view.WindowCompat
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,6 +14,10 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 // 548cd9aee7828f5f4642b9c341716422
 class MainActivity : AppCompatActivity() {
@@ -22,21 +28,63 @@ class MainActivity : AppCompatActivity() {
 // To display the activity in full screen and also make the status bar and notification bar transparent in themes.xml
 
 
-        fetchWeatherData()
+        fetchWeatherData("ahmedabad")
     }
 
-    private fun fetchWeatherData() {
+    private fun fetchWeatherData(cityName : String) {
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl("https://api.openweathermap.org/data/2.5/")
             .build().create(ApiInterface::class.java)
-        val response = retrofit.getWeatherData(city = "ahmedabad", "548cd9aee7828f5f4642b9c341716422", units = "metric")
+        val response = retrofit.getWeatherData(cityName, "548cd9aee7828f5f4642b9c341716422", units = "metric")
         response.enqueue(object : Callback<WeatherApp>{
             override fun onResponse(call: Call<WeatherApp>, response: Response<WeatherApp>) {
                 val responseBody = response.body()
                 if (response.isSuccessful && responseBody != null){
                     val temperature = responseBody.main.temp.toString()
-                    Log.d( "TAG", "onResponse: $temperature")
+                    val condition = responseBody.weather.firstOrNull()?.main?: "unknown"
+                    val highTemp = responseBody.main.temp_max
+                    val lowTemp = responseBody.main.temp_min
+                    val humidity = responseBody.main.humidity
+                    val windSpeed = responseBody.wind.speed
+                    val sunRise = responseBody.sys.sunrise
+                    val sunSet = responseBody.sys.sunset
+                    val seaLevel = responseBody.main.pressure
+
+                    // Find the TextView with ID "temp"
+                    val tempTextView = findViewById<TextView>(R.id.temprature)
+                    val condTextView = findViewById<TextView>(R.id.cond)
+                    val highTempTextView = findViewById<TextView>(R.id.high_temp)
+                    val lowTempTextView = findViewById<TextView>(R.id.low_temp)
+                    val humidityTextView = findViewById<TextView>(R.id.humidity)
+                    val windSpeedTextView = findViewById<TextView>(R.id.windspeed)
+                    val sunRiseTextView = findViewById<TextView>(R.id.sunrisee)
+                    val sunSetTextView = findViewById<TextView>(R.id.sunset)
+                    val seaLevelTextView = findViewById<TextView>(R.id.sea)
+                    val dayTextView = findViewById<TextView>(R.id.day)
+                    val dateTextView = findViewById<TextView>(R.id.date)
+                    val cityNameTextView = findViewById<TextView>(R.id.cityName)
+
+                    // Set the variable values to the TextView
+                    tempTextView.text = "$temperature°"
+                    condTextView.text = condition
+                    highTempTextView.text = "H:$highTemp°"
+                    lowTempTextView.text = "L:$lowTemp°"
+                    humidityTextView.text = "$humidity %"
+                    windSpeedTextView.text = "$windSpeed m/s"
+                    sunRiseTextView.text = "$sunRise"
+                    sunSetTextView.text = "$sunSet"
+                    seaLevelTextView.text = "$seaLevel hpa"
+                    condTextView.text = condition
+                    dayTextView.text = dayName(System.currentTimeMillis())
+                    dateTextView.text = date()
+                    cityNameTextView.text = "$cityName"
+
+
+
+
+
+//                    Log.d( "TAG", "onResponse: $temperature")
                 }
             }
 
@@ -45,5 +93,17 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+
+    }
+
+    private fun date(): String {
+        val sdf = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+        return sdf.format((Date()))
+    }
+
+    fun dayName(timestamp: Long): String {
+        val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
+        return sdf.format((Date()))
     }
 }
